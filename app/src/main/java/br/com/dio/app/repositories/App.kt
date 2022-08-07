ee65cabf694d27ec76be43114bba6f5128351acf
bernardobb <bernardobb.eletronica@gmail.com>
2022-08-07 19:11:21 -0300

    app de visualização de repositorios github finalizado

--- app/src/main/java/br/com/dio/app/repositories/App.kt
+++ app/src/main/java/br/com/dio/app/repositories/App.kt
@@ -1,6 +1,10 @@
 package br.com.dio.app.repositories
 
 import android.app.Application
+import android.app.Presentation
+import br.com.dio.app.repositories.data.di.DataModule
+import br.com.dio.app.repositories.domain.di.DomainModule
+import br.com.dio.app.repositories.presentation.di.PresentationModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -12,6 +16,8 @@ class App : Application() {
         startKoin {
             androidContext(this@App)
         }
-
+        DataModule.load()
+        DomainModule.load()
+        PresentationModule.load()
     }
 }
